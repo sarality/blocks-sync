02082cfb351407b7e1b7882553db03725b1fcabf
Satya <puniani@gmail.com>
2017-08-04 17:42:43 +0530

    updated interface to remove message/timestamp

--- src/main/java/com/sarality/sync/SyncErrorLogger.java
+++ src/main/java/com/sarality/sync/SyncErrorLogger.java
@@ -1,7 +1,5 @@
 package com.sarality.sync;
 
-import hirondelle.date4j.DateTime;
-
 /**
  * Log syncing errors
  *
@@ -12,7 +10,6 @@ public interface SyncErrorLogger {
 
   public void logError(Exception e);
 
-  public void logError(Exception e, Long errorRecordId, String errorTableName, DateTime errorTimeStamp,
-      String errorDescription);
+  public void logError(Exception e, Long syncRecordId, String syncTableName);
 
 }
