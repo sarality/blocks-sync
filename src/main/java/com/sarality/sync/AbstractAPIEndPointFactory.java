312450cb53ad743b78c44e04e79156414a330a33
Satya <puniani@gmail.com>
2017-09-19 18:48:10 +0530

    accept a http request initializer

--- src/main/java/com/sarality/sync/AbstractAPIEndPointFactory.java
+++ src/main/java/com/sarality/sync/AbstractAPIEndPointFactory.java
@@ -1,6 +1,7 @@
 package com.sarality.sync;
 
 import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClient;
+import com.google.api.client.http.HttpRequestInitializer;
 
 /**
  * Factory for generating a doThat API Endpoint object
@@ -8,8 +9,8 @@ import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClient;
  * @author Satya@ (Satya Puniani)
  */
 
-public interface AbstractAPIEndPointFactory<T extends AbstractGoogleJsonClient> {
+public interface AbstractAPIEndPointFactory<T extends AbstractGoogleJsonClient, H extends HttpRequestInitializer> {
 
-  public T getAPIEndPoint();
+  public T getAPIEndPoint(H initializer);
 
 }
